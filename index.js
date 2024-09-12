a015efe4c6e18aef30c9d4b7b37d6f4f621c9554
S0330B <sushilsharmasubedi@gmail.com>
2024-09-12 15:25:20 +0545

    Design: Integrate and improvise frontend design

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+const path = require("path");
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
 app.listen(3000, async (req, res) => {
   console.log("App is running on port 3000");
 });
